976323373240fa5e0bea0fa9f9190ca81ca68e51
Scott Bellware <sbellware@gmail.com>
2022-10-27 15:31:36 -0500

    Test is clarified by calling the class interface rather than building an instance

--- test/automated/until/no_block_error.rb
+++ test/automated/until/no_block_error.rb
@@ -2,11 +2,9 @@ require_relative '../automated_init'
 
 context "Until" do
   context "Actuate Without Block" do
-    cycle = Until.build
-
     test "Is an error" do
       assert_raises(Until::NoBlockError) do
-        cycle.()
+        Until.()
       end
     end
   end
