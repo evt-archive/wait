641719425976e467d2680ea09b749e93b7e13582
Scott Bellware <sbellware@gmail.com>
2022-10-27 14:33:23 -0500

    No block error

--- test/automated/until/no_block_error.rb
+++ test/automated/until/no_block_error.rb
@@ -5,7 +5,7 @@ context "Until" do
     cycle = Until.build
 
     test "Is an error" do
-      assert_raises(Until::Error) do
+      assert_raises(Until::NoBlockError) do
         cycle.()
       end
     end
